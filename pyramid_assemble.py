71fcf698af9db2cbb7f9ba0371caef4546f39a9e
John Hoffer <john@hoff.in>
2020-11-10 10:38:38 -0500

    no need to convert dtype if is_mask

--- pyramid_assemble.py
+++ pyramid_assemble.py
@@ -32,7 +32,7 @@ def preduce(coords, img_in, img_out, is_mask):
     else:
         tile = skimage.img_as_float32(img_in[iy1:iy2, ix1:ix2])
         tile = skimage.transform.downscale_local_mean(tile, (2, 2))
-    tile = dtype_convert(tile, img_out.dtype)
+        tile = dtype_convert(tile, img_out.dtype)
     img_out[oy1:oy2, ox1:ox2] = tile
 
 
