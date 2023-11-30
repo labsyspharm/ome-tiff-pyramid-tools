4d8d63c7c86d9def4cdc851f60e93be75e299c40
Jeremy Muhlich <jmuhlich@bitflood.org>
2023-11-30 01:21:18 -0500

    Write proper BF6 pyramids; support multi-page input
    * Remove old legacy pyramid output code and replace it with the native
      tifffile OME-TIFF pyramid output support.
    * Allow multi-page files for input, including single-page specifier
      like tiffcp (filename.tif,x for page x).

--- pyramid_assemble.py
+++ pyramid_assemble.py
@@ -24,106 +24,8 @@ except ImportError:
     from skimage.util.dtype import convert as dtype_convert
 
 
-def preduce(coords, img_in, img_out, is_mask):
-    (iy1, ix1), (iy2, ix2) = coords
-    (oy1, ox1), (oy2, ox2) = np.array(coords) // 2
-    if is_mask:
-        tile = img_in[iy1:iy2:2, ix1:ix2:2]
-    else:
-        tile = skimage.img_as_float32(img_in[iy1:iy2, ix1:ix2])
-        tile = skimage.transform.downscale_local_mean(tile, (2, 2))
-        tile = dtype_convert(tile, img_out.dtype)
-    img_out[oy1:oy2, ox1:ox2] = tile
-
-
-def imsave(path, img, tile_size, **kwargs):
-    tifffile.imwrite(
-        path, img, bigtiff=True, append=True, tile=(tile_size, tile_size),
-        ome=False, imagej=False, metadata=None, compression="adobe_deflate",
-        predictor=True, **kwargs
-    )
-
-
 def format_shape(shape):
-    return "%dx%d" % (shape[1], shape[0])
-
-
-def construct_xml(filename, shapes, num_channels, ome_dtype, pixel_size=1):
-    img_uuid = uuid.uuid4().urn
-    ifd = 0
-    xml = io.StringIO()
-    xml.write(u'<?xml version="1.0" encoding="UTF-8"?>')
-    xml.write(
-        (u'<OME xmlns="http://www.openmicroscopy.org/Schemas/OME/2016-06"'
-         ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
-         ' UUID="{uuid}"'
-         ' xsi:schemaLocation="http://www.openmicroscopy.org/Schemas/OME/2016-06'
-         ' http://www.openmicroscopy.org/Schemas/OME/2016-06/ome.xsd">')
-        .format(uuid=img_uuid)
-    )
-    for level, shape in enumerate(shapes):
-        if level == 0:
-            psize_xml = (
-                u'PhysicalSizeX="{0}" PhysicalSizeXUnit="\u00b5m"'
-                u' PhysicalSizeY="{0}" PhysicalSizeYUnit="\u00b5m"'
-                .format(pixel_size)
-            )
-        else:
-            psize_xml = u''
-        xml.write(u'<Image ID="Image:{}">'.format(level))
-        xml.write(
-            (u'<Pixels BigEndian="false" DimensionOrder="XYZCT"'
-             ' ID="Pixels:{level}" {psize_xml} SizeC="{num_channels}" SizeT="1"'
-             ' SizeX="{sizex}" SizeY="{sizey}" SizeZ="1" Type="{ome_dtype}">')
-            .format(
-                level=level, psize_xml=psize_xml, num_channels=num_channels,
-                sizex=shape[1], sizey=shape[0], ome_dtype=ome_dtype
-            )
-        )
-        for channel in range(num_channels):
-            xml.write(
-                (u'<Channel ID="Channel:{level}:{channel}"'
-                 + (u' Name="Channel {channel}"' if level == 0 else u'')
-                 + u' SamplesPerPixel="1"><LightPath/></Channel>')
-                .format(level=level, channel=channel)
-            )
-        for channel in range(num_channels):
-            xml.write(
-                (u'<TiffData FirstC="{channel}" FirstT="0" FirstZ="0"'
-                 ' IFD="{ifd}" PlaneCount="1">'
-                 '<UUID FileName="{filename}">{uuid}</UUID>'
-                 '</TiffData>')
-                .format(
-                    channel=channel, ifd=ifd, filename=filename, uuid=img_uuid
-                )
-            )
-            ifd += 1
-        if level == 0:
-            for channel in range(num_channels):
-                xml.write(
-                    u'<Plane TheC="{channel}" TheT="0" TheZ="0"/>'
-                    .format(channel=channel)
-                )
-        xml.write(u'</Pixels>')
-        xml.write(u'</Image>')
-    xml.write(u'</OME>')
-    xml_bytes = xml.getvalue().encode('utf-8') + b'\x00'
-    return xml_bytes
-
-
-def patch_ometiff_xml(path, xml_bytes):
-    with open(path, 'rb+') as f:
-        f.seek(0, io.SEEK_END)
-        xml_offset = f.tell()
-        f.write(xml_bytes)
-        f.seek(0)
-        ifd_block = f.read(500)
-        match = re.search(b'!!xml!!\x00', ifd_block)
-        if match is None:
-            raise RuntimeError("Did not find placeholder string in IFD")
-        f.seek(match.start() - 8)
-        f.write(struct.pack('<Q', len(xml_bytes)))
-        f.write(struct.pack('<Q', xml_offset))
+    return "%d x %d" % (shape[1], shape[0])
 
 
 def error(path, msg):
@@ -133,25 +35,26 @@ def error(path, msg):
 
 def main():
 
-    tile_size = 1024
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "in_paths", metavar="input.tif", type=pathlib.Path, nargs="+",
         help="List of TIFF files to combine. All images must have the same"
-        " dimensions and pixel type.",
+        " dimensions and pixel type. All pages of multi-page images will be"
+        " included by default; the suffix ,p may be appended to the filename to"
+        " specify a single page p.",
     )
     parser.add_argument(
         "out_path", metavar="output.ome.tif", type=pathlib.Path,
         help="Output filename. Script will exit immediately if file exists.",
     )
     parser.add_argument(
-        "--pixel-size", metavar="SIZE", type=float, default=1.0,
-        help="Pixel size in microns; default is 1.0",
+        "--pixel-size", metavar="MICRONS", type=float, default=None,
+        help="Pixel size in microns. Will be recorded in OME-XML metadata.",
     )
     parser.add_argument(
         "--tile-size", metavar="PIXELS", type=int, default=1024,
-        help="Width of pyramid tiles in output file; default is 1024",
+        help="Width of pyramid tiles in output file (must be a multiple of 16);"
+        " default is 1024",
     )
     parser.add_argument(
         "--mask", action="store_true", default=False,
@@ -180,13 +83,34 @@ def main():
             " count."
         )
         print()
+    tifffile.TIFF.MAXWORKERS = args.num_threads
+    tifffile.TIFF.MAXIOWORKERS = args.num_threads * 5
 
-    print("Appending input images")
-    for i, path in enumerate(in_paths):
-        print(f"    {i+1}: {path}")
-        img_in = tifffile.imread(path)
-        if i == 0:
-            base_shape = img_in.shape
+    in_imgs = []
+    print("Scanning input images")
+    for i, path in enumerate(in_paths, 1):
+        spath = str(path)
+        if match := re.search(r",(\d+)$", spath):
+            c = int(match.group(1))
+            path = pathlib.Path(spath[:match.start()])
+        else:
+            c = None
+        img_in = zarr.open(tifffile.imread(path, key=c, level=0, aszarr=True))
+        if img_in.ndim == 2:
+            shape = img_in.shape
+            imgs = [img_in]
+        elif img_in.ndim == 3:
+            shape = img_in.shape[1:]
+            imgs = [
+                zarr.open(tifffile.imread(path, key=i, level=0, aszarr=True))
+                for i in range(img_in.shape[0])
+            ]
+        else:
+            error(
+                path, f"{img_in.ndim}-dimensional images are not supported",
+            )
+        if i == 1:
+            base_shape = shape
             dtype = img_in.dtype
             if dtype == np.uint32:
                 if not is_mask:
@@ -196,27 +120,20 @@ def main():
                        " Please contact the authors if you need support for"
                        " intensity-based uint32 images."
                     )
-                ome_dtype = 'uint32'
-            elif dtype == np.uint16:
-                ome_dtype = 'uint16'
-            elif dtype == np.uint8:
-                ome_dtype = 'uint8'
+            elif dtype == np.uint16 or dtype == np.uint8:
+                pass
             else:
                 error(
                     path,
                     f"Can't handle dtype '{dtype}' yet, please contact the"
                     f" authors."
                 )
-            kwargs = {
-                'description': '!!xml!!',
-                'software': 'Glencoe/Faas pyramid'
-            }
         else:
-            if img_in.shape != base_shape:
+            if shape != base_shape:
                 error(
                     path,
                     f"Expected shape {base_shape} to match first input image,"
-                    f" got {img_in.shape} instead."
+                    f" got {shape} instead."
                 )
             if img_in.dtype != dtype:
                 error(
@@ -224,15 +141,17 @@ def main():
                     f"Expected dtype '{dtype}' to match first input image,"
                     f" got '{img_in.dtype}' instead."
                 )
-            kwargs = {}
-        imsave(out_path, img_in, tile_size, **kwargs)
-        del img_in
+        print(f"    file {i}")
+        print(f"        path: {spath}")
+        print(f"        properties: shape={img_in.shape} dtype={img_in.dtype}")
+        in_imgs.extend(imgs)
     print()
 
-    num_channels = len(in_paths)
-    num_levels = np.ceil(np.log2(max(base_shape) / tile_size)) + 1
+    num_channels = len(in_imgs)
+    num_levels = np.ceil(np.log2(max(base_shape) / args.tile_size)) + 1
     factors = 2 ** np.arange(num_levels)
-    shapes = (np.ceil(np.array(base_shape) / factors[:,None])).astype(int)
+    shapes = np.ceil(np.array(base_shape) / factors[:,None]).astype(int)
+    cshapes = np.ceil(shapes / args.tile_size).astype(int)
 
     print("Pyramid level sizes:")
     for i, shape in enumerate(shapes):
@@ -242,47 +161,73 @@ def main():
         print()
     print()
 
-    executor = concurrent.futures.ThreadPoolExecutor(args.num_threads)
+    pool = concurrent.futures.ThreadPoolExecutor(args.num_threads)
 
-    shape_pairs = zip(shapes[:-1], shapes[1:])
-    for level, (shape_in, shape_out) in enumerate(shape_pairs):
+    def tiles0():
+        ts = args.tile_size
+        ch, cw = cshapes[0]
+        for c, zimg in enumerate(in_imgs, 1):
+            print(f"    channel {c}")
+            img = zimg[:]
+            for j in range(ch):
+                for i in range(cw):
+                    tile = img[ts * j : ts * (j + 1), ts * i : ts * (i + 1)]
+                    yield tile
+            del img
 
-        print("Resizing channels for level {} ({} -> {})".format(
-            level + 2, format_shape(shape_in), format_shape(shape_out)
-        ))
+    def tiles(level):
+        tiff_out = tifffile.TiffFile(args.out_path, is_ome=False)
+        zimg = zarr.open(tiff_out.series[0].aszarr(level=level - 1))
+        ts = args.tile_size * 2
 
-        ty = np.array(range(0, shape_in[0], tile_size))
-        tx = np.array(range(0, shape_in[1], tile_size))
-        coords = list(zip(
-            itertools.product(ty, tx),
-            itertools.product(ty + tile_size, tx + tile_size)
-        ))
-        img_out = np.empty(shape_out, dtype)
+        def tile(coords):
+            c, j, i = coords
+            tile = zimg[c, ts * j : ts * (j + 1), ts * i : ts * (i + 1)]
+            tile = skimage.transform.downscale_local_mean(tile, (2, 2))
+            tile = np.round(tile).astype(dtype)
+            return tile
 
-        for c in range(num_channels):
+        ch, cw = cshapes[level]
+        coords = itertools.product(range(num_channels), range(ch), range(cw))
+        yield from pool.map(tile, coords)
 
-            tiff = tifffile.TiffFile(out_path)
-            page = level * num_channels + c
-            img_in = zarr.open(tiff.aszarr(key=page), mode="r")
-            for i, _ in enumerate(executor.map(
-                preduce, coords,
-                itertools.repeat(img_in), itertools.repeat(img_out),
-                itertools.repeat(is_mask)
-            )):
-                percent = int((i + 1) / len(coords) * 100)
-                if i % 20 == 0 or percent == 100:
-                    print(f"\r    {c+1}: {percent}%", end="")
-                    sys.stdout.flush()
-            tiff.close()
-            imsave(out_path, img_out, tile_size)
-            print()
-
+    metadata = {
+        "UUID": uuid.uuid4().urn,
+    }
+    if args.pixel_size:
+        metadata.update({
+            "PhysicalSizeX": args.pixel_size,
+            "PhysicalSizeXUnit": "µm",
+            "PhysicalSizeY": args.pixel_size,
+            "PhysicalSizeYUnit": "µm",
+        })
+    print(f"Writing level 1: {format_shape(shapes[0])}")
+    with tifffile.TiffWriter(args.out_path, ome=True, bigtiff=True) as writer:
+        writer.write(
+            data=tiles0(),
+            shape=(num_channels,) + tuple(shapes[0]),
+            subifds=num_levels - 1,
+            dtype=dtype,
+            tile=(args.tile_size, args.tile_size),
+            compression="adobe_deflate",
+            predictor=True,
+            metadata=metadata,
+        )
         print()
-
-    xml = construct_xml(
-        os.path.basename(out_path), shapes, num_channels, ome_dtype, args.pixel_size
-    )
-    patch_ometiff_xml(out_path, xml)
+        for level, shape in enumerate(shapes[1:], 1):
+            print(
+                f"Resizing image for level {level + 1}: {format_shape(shape)}"
+            )
+            writer.write(
+                data=tiles(level),
+                shape=(num_channels,) + tuple(shape),
+                subfiletype=1,
+                dtype=dtype,
+                tile=(args.tile_size, args.tile_size),
+                compression="adobe_deflate",
+                predictor=True,
+            )
+        print()
 
 
 if __name__ == '__main__':
